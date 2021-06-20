4f219510d46a9e1844a7ca889a9eddfb0b914c14
Jehad <jehadwael.jj@gmail.com>
2021-06-21 00:03:24 +0300

    .

--- src/App.js
+++ src/App.js
@@ -56,13 +56,7 @@ class App extends React.Component {
           <main>
             <div className="content">
               <div className="main">
-                <Filter
-                  count={this.state.products.length}
-                  size={this.state.size}
-                  sort={this.state.sort}
-                  filterProducts={this.filterProducts}
-                  sortProducts={this.sortProducts}
-                ></Filter>
+                <Filter></Filter>
                 <Products addToCart={this.addToCart}></Products>
               </div>
               <div className="sidebar">
